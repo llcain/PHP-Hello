9354dce3462754369b72ad4f959b954f9a9cbb97
llcain <llcain2012@gmail.com>
2024-03-31 03:40:03 -0500

    Print Hello World with echo

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-echo "Hello World!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact form</title>
+</head>
+<body>
+    
+    
+
+    <?php
+    echo "Hello LaQuisha!!";
+    ?>
+</body>
+</html>
